a66c255242427443fb94681a006bfb1b619fdae3
Aranda Morrison <aranda_morrison@yahoo.com.au>
2012-10-26 23:05:40 +0800

    Improvements to the iOS Phone/Tasks implementations. Still not tested.

---
+++ Microsoft.Devices/iOS/Phone/Tasks/ContentIdHelper.cs
@@ -0,0 +1,30 @@
+﻿using MonoTouch.UIKit;
+using MonoTouch.Foundation;
+
+namespace Microsoft.Phone.Tasks
+{
+    internal static class ContentIdHelper
+    {
+        // Gets the safe bundle name from the current bundle. This can be 
+        // used as part of a itms:// url to open the app store
+        internal static string GetCurrentContentIdentifier()
+        {
+            string bundleName = NSBundle.MainBundle.InfoDictionary[new NSString("CFBundleName")].ToString();
+            return StripUnsafeCharacters(bundleName);
+        }
+        
+        // Removes any characters that are not letters or digits from the input string
+        internal static string StripUnsafeCharacters(string input)
+        {
+            System.Text.StringBuilder output = new System.Text.StringBuilder();
+            foreach (char c in input)
+            {
+                if (!char.IsLetterOrDigit(c))
+                    continue;
+
+                output.Append(c);
+            }
+            return output.ToString();
+        }
+    }
+}
--- Microsoft.Devices/iOS/Phone/Tasks/MarketplaceDetailTask.cs
+++ Microsoft.Devices/iOS/Phone/Tasks/MarketplaceDetailTask.cs
@@ -12,21 +12,34 @@ namespace Microsoft.Phone.Tasks
     /// </summary>
     public sealed class MarketplaceDetailTask
     {
-        // Appstore bundle name, or content identifier. When using the bundle name
+        // Appstore bundle name, or other content identifier. When using the bundle name
         // you must remove all white space and punctuation characters.
         public string ContentIdentifier { get; set; }
 
-        // Ignored by this implementation
+        // The content type. If MarketplaceContentType.Music is used,
+        // The ContentIdentifier is assumed to be for an album
         public MarketplaceContentType ContentType { get; set; }
 
         public void Show()
         {
-            if (ContentIdentifier == null)
+            NSUrl appStoreUrl;
+            if (ContentType == MarketplaceContentType.Applications)
             {
-                ContentIdentifier = NSBundle.MainBundle.BundleIdentifier;
+                if (ContentIdentifier == null)
+                {
+                    ContentIdentifier = ContentIdHelper.GetCurrentContentIdentifier();
+                }
+                appStoreUrl = new NSUrl("itms-apps://itunes.com/app/" + ContentIdentifier);
+            }
+            else
+            {
+                if (ContentIdentifier == null)
+                {
+                    throw new System.InvalidOperationException("Can not open MarketplaceContentType.Music URL without an explicit ContentIdentifier");
+                }
+                appStoreUrl = new NSUrl("itms://itunes.com/album/" + ContentIdentifier);
             }
 
-            NSUrl appStoreUrl = new NSUrl("itms-apps://itunes.com/app/" + ContentIdentifier);
             UIApplication.SharedApplication.OpenUrl(appStoreUrl);
         }
     }
